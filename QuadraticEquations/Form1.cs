b8f30e19c0ef93f315b30d90c935ed851987a536
MaegPi <jeska.vandetta@gmail.com>
2015-05-03 21:23:30 +0100

    Import
    Quadratic equations in Visual C#

---
+++ QuadraticEquations/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuadraticEquations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a =  Double.Parse(textBox1.Text);
+            double b = Double.Parse(textBox2.Text);
+            double c = Double.Parse(textBox3.Text);
+            double bSqu = Math.Pow(b, 2);
+            double fourAC = Math.Pow((bSqu - (4 * a * c)), .5);
+            double xPositive = ((-1*b)+fourAC)/(2*a);
+            double xNegative = ((-1*b) - fourAC) / (2 * a);
+            label5.Text = "x is " + xPositive.ToString() + " or " + xNegative.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
